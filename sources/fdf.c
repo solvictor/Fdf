86cca9ed70ed31518df729b6ac4f5db819cc35b6
Victor Egret <victor.egret.pro@gmail.com>
2022-11-29 17:58:34 +0100

    Display WIP

--- sources/fdf.c
+++ sources/fdf.c
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 12:50:15 by vegret            #+#    #+#             */
-/*   Updated: 2022/11/29 00:33:12 by vegret           ###   ########.fr       */
+/*   Updated: 2022/11/29 17:56:53 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,15 +45,16 @@ static void	link_points(int x0, int y0, int x1, int y1, t_vars *vars)
 	}
 }
 
-#define MIN_X 40
-#define MIN_Y 40
-#define DISTANCE 25
+#define MIN_X 300
+#define MIN_Y 300
+#define DISTANCE 20
 
 void	calibrate(t_point *point)
 {
 	point->x = point->x * DISTANCE + MIN_X;
 	point->y = point->y * DISTANCE + MIN_Y;
 }
+
 static void	printtest(t_point *content)
 {
 	printf(
@@ -64,23 +65,70 @@ static void	printtest(t_point *content)
 		content->color);
 }
 
+static void	get_coords(int x0, int y0, t_point *point)
+{
+	point->x = x0 + 0.86602540378 * DISTANCE;
+	point->y = y0 + DISTANCE / 2;
+}
+
+static void	make_x(t_points *points)
+{
+	t_points	*point;
+	t_points	*tmp;
+	int			x0;
+	int			y0;
+	int			currx;
+	int			i;
+
+	x0 = MIN_X;
+	currx = MIN_X;
+	y0 = MIN_Y;
+	point = points;
+	i = 1;
+	while (point)
+	{
+		get_coords(x0, y0, point);
+		if (point->next && point->next->data.x != currx)
+		{
+			currx = point->next->data.x;
+			x0 = MIN_X;
+			y0 = MIN_Y;
+			i++;
+		}
+		else
+		{
+			x0 = point->data.x;
+			y0 = point->data.y;
+		}
+		point = point->next;
+	}
+}
+
 static int	putpoints(t_vars *vars)
 {
 	t_points	*point;
+	t_points	*tmp;
 
 	lstiter(vars->points, &calibrate);
+	make_x(vars->points);
 	lstiter(vars->points, &printtest);
 	point = vars->points;
 	while (point)
 	{
 		mlx_pixel_put(vars->mlx, vars->win, point->data.x, point->data.y, DEFAULT_POINT_COLOR);
-		if (point->next && point->next->data.x == point->data.x)
-			link_points(point->next->data.x, point->next->data.y, point->data.x, point->data.y, vars);
+		//if (point->next /*&& point->next->data.x == point->data.x*/)
+		//	link_points(point->next->data.x, point->next->data.y, point->data.x, point->data.y, vars);
+		//tmp = point->next;
+		//while (tmp)
+		//{
+		//	if (tmp->data.y == point->data.y)
+		//		link_points(tmp->data.x, tmp->data.y, point->data.x, point->data.y, vars);
+		//	tmp = tmp->next;
+		//}
 		point = point->next;
 	}
 	return (0);
 }
-
 
 /* TODO
 Makefile
@@ -99,6 +147,7 @@ int	main(int argc, char *argv[])
 		ft_putendl_fd(" <filename>", 1);
 		return (1);
 	}
+	// Check if path endswith .fdf
 	fd = open(argv[1], O_RDONLY);
 	vars.points = parse_map(fd);
 	if (!vars.points)
