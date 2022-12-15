e9c83c8710b00854d95879ffa88524e2a2d05639
Victor Egret <victor.egret.pro@gmail.com>
2022-12-15 13:00:09 +0100

    Initialized value fix

--- sources/fdf.c
+++ sources/fdf.c
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 12:50:15 by vegret            #+#    #+#             */
-/*   Updated: 2022/12/15 12:56:44 by vegret           ###   ########.fr       */
+/*   Updated: 2022/12/15 12:59:51 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ int	main(int argc, char *argv[])
 	vars.id = NULL;
 	vars.img.id = NULL;
 	vars.win = NULL;
+	vars.title = NULL;
 	fdf_init(fd, argv[1], &vars);
 	mlx_mouse_hook(vars.win, &on_mouse, &vars);
 	mlx_hook(vars.win, DestroyNotify, StructureNotifyMask, &on_destroy, &vars);
